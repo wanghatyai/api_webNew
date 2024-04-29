63e64e9a10c2014a2e7069d821728dc2b406bb07
WangHatyaiDev <wanghatyai@gmail.com>
2024-04-29 18:47:05 +0700

    Update banner-slider.php

--- banner-slider.php
+++ banner-slider.php
@@ -1,4 +1,8 @@
 <?php
+  header("Access-Control-Allow-Origin: *");
+  header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+  header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
   require_once('../../Akitokung/00-connection.class.sqli.php');
 
   if ($_SERVER['REQUEST_METHOD'] == 'GET') {
